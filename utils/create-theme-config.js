9c5d8d3de74de7707660df6c2cfb820569ca605a
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-05-19 13:03:28 -0400

    Se ha actualizado el archivo de configuración del tema

--- utils/create-theme-config.js
+++ utils/create-theme-config.js
@@ -2,22 +2,17 @@
  * Crea el archivo theme.config.js con informacion básica y necesaria para trabajar con el tema
  */
 module.exports = function (name) {
-return `
-export default {
-    /**
-     * Information
-     */
-    theme: {
-        name: "${name}",
-      description: "A starter blogger template with BloggerBase",
-      version: "1.0.0",
-      url: "https://github.com/Karasu-themes/BloggerBase",
-      license: "Release under MIT License",
-      author: {
-        name: "MarceloTLD",
-        url: "https://github.com/MarceloTLD"
-      },
-    }
+  return `
+export const themeConfig = {
+  name: "${name}",
+  description: "A basic tool for building blogger template",
+  version: "1.0.0",
+  url: "https://github.com/Karasu-themes/BloggerBase",
+  license: "Release under MIT License",
+  author: {
+    name: "MarceloTLD",
+    url: "https://github.com/MarceloTLD"
+  },
 }
 `.trim();
 }
