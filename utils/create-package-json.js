9d5d18c989de2ae778f79b9fededc87fd1b400ff
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-05-19 13:03:47 -0400

    Se ha actualizado el archivo de configuración para el package.json

--- utils/create-package-json.js
+++ utils/create-package-json.js
@@ -7,7 +7,7 @@ module.exports = function (name, template = "Blank") {
     "name": "${name}",
     "version": "1.0.0",
     "type": "module",
-    "description": "My awesome blogger template with ${template.toLowerCase}",
+    "description": "My awesome blogger template with ${template}",
     "scripts": {
         "build": "blogger build --mode production",
         "start": "blogger start",
@@ -17,18 +17,19 @@ module.exports = function (name, template = "Blank") {
     "author": "",
     "license": "MIT",
     "devDependencies": {
-        "bloggerbase": "^2.0.0",
-        "cheerio": "1.0.0-rc.12",
-        "chokidar": "3.5.3",
-        "ejs": "3.1.9",
-        "postcss": "8.4.31",
-        "rollup": "4.5.1",
-        "rollup-plugin-esbuild": "6.1.0",
-        "sass": "1.69.5"
+        "clean-css": "^5.3.3",
+        "postcss": "^8.4.31",
+        "rollup": "^4.5.1",
+        "sass": "^1.69.5",
+        "autoprefixer": "^10.4.19",
+        "ejs": "^3.1.9",
+        "terser": "^5.30.2"
     },
     "dependencies": {
+        "bloggerbase": "^2.0.0",
+        "chokidar": "^3.5.3",
         "chalk": "^5.3.0",
-        "commander": "12.0.0"
+        "commander": "^12.0.0"
     }
 }
     `.trim();
